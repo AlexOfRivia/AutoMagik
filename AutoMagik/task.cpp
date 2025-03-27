1b9c19ce5d3d6cbe719a8ecfffdcecc23ca258dc
Alexander Bhandari <31488157+AlexOfRivia@users.noreply.github.com>
2025-03-27 15:01:56 +0100

    changes to the task class

--- AutoMagik/task.cpp
+++ AutoMagik/task.cpp
@@ -12,3 +12,63 @@ Task::~Task()
 {
 
 }
+
+//Setting the instructions for the task
+void Task::setTaskInstructions(std::string instructions)
+{
+
+}
+
+//Setting the parts needed for the task
+void Task::setPartsNeeded(std::string parts)
+{
+
+}
+
+//Setting the comments for the task
+void Task::setComments(std::string comments)
+{
+
+}
+
+//Setting the ID for the task
+void Task::setTaskID(int ID)
+{
+
+}
+
+//Setting the car for the task
+void Task::setTaskCar()
+{
+
+}
+
+//Getting the instructions for the task
+std::string Task::getTaskInstructions()
+{
+	return std::string();
+}
+
+//Getting the parts needed for the task
+std::string Task::getPartsNeeded()
+{
+	return std::string();
+}
+
+//Getting the comments for the task
+std::string Task::getComments()
+{
+	return std::string();
+}
+
+//Getting the ID for the task
+int Task::getTaskID()
+{
+	return 0;
+}
+
+//Getting the car for the task
+Car Task::getTaskCar()
+{
+	return Car();
+}
--- AutoMagik/task.h
+++ AutoMagik/task.h
@@ -7,6 +7,20 @@ class Task
 public:
 	Task(); //Constructor
 	~Task(); //Destructor
+
+	void setTaskInstructions(std::string instructions); //Set the instructions for the task
+	void setPartsNeeded(std::string parts); //Set the parts needed for the task
+	void setComments(std::string comments); //Set the comments for the task
+	void setTaskID(int ID); //Set the ID for the task
+	void setTaskCar(); //Set the car for the task
+
+	std::string getTaskInstructions(); //Get the instructions for the task
+	std::string getPartsNeeded(); //Get the parts needed for the task
+	std::string getComments(); //Get the comments for the task
+	int getTaskID(); //Get the ID for the task
+	Car getTaskCar(); //Get the car for the task
+
+
 private:
 	std::string taskInstructions; //Instructions for the task
 	std::string partsNeeded; //Parts needed for the task
