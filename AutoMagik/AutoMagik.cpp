2cec17853750ed158beaf5a0ab4c37d99cf4d754
Alexander Bhandari <31488157+AlexOfRivia@users.noreply.github.com>
2025-03-27 14:11:32 +0100

    Added some initial functionality to UI

--- AutoMagik/AutoMagik.cpp
+++ AutoMagik/AutoMagik.cpp
@@ -4,6 +4,26 @@ AutoMagik::AutoMagik(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    //Showing the managerDashboardPage 
+    QObject::connect(
+        ui.managerModeButton, &QPushButton::clicked, [this]() { ui.stackedWidget->setCurrentIndex(1); }
+    );
+
+	//Showing the workerDashboardPage
+    QObject::connect(
+        ui.workerModeButton, &QPushButton::clicked, [this]() { ui.stackedWidget->setCurrentIndex(2); }
+    );
+
+	//Returning to the main page
+    QObject::connect(
+        ui.workerBackButton, &QPushButton::clicked, [this]() { ui.stackedWidget->setCurrentIndex(0); }
+    );
+
+    //Returning to the main page
+    QObject::connect(
+        ui.managerBackButton, &QPushButton::clicked, [this]() { ui.stackedWidget->setCurrentIndex(0); }
+    );
 }
 
 AutoMagik::~AutoMagik()
